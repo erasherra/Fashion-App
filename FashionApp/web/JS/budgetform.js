cf1eabb4520587269697db6774d5007eb0fe0f38
Unknown <sami.haapanen@hotmail.com>
2018-02-07 22:47:26 +0200

    Edited form.html, still work in progress

--- FashionApp/web/JS/budgetform.js
+++ FashionApp/web/JS/budgetform.js
@@ -1,22 +1,8 @@
-//let nt = document.querySelector('#newTable');
-//let tbldiv = document.querySelector('#tbldiv');
-let tbl = document.createElement("table");
 
-document.addEventListener("DOMContentLoaded", function(){   
-        let tbl = document.createElement("table");
-        let tr = document.createElement("tr");
-        let th = document.createElement("th");
-        let txt = document.createTextNode("hai!");
-        let th2 = document.createElement("th");
-        let txt2 = document.createTextNode("haiiii!");
-        th.appendChild(txt);
-        th2.appendChild(txt2);
-        tr.appendChild(th);
-        tr.appendChild(th2);
-        tbl.appendChild(tr);
-        document.querySelector('#newTable').addEventListener("click", function(){
-        document.querySelector('#tbldiv').appendChild(tbl);
-        
+document.addEventListener("DOMContentLoaded", function () {
+    
+    document.querySelector('#newTable').addEventListener("click", function () {
+    
     });
 
 });
