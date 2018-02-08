bccb6abaf65451c36f3bf816f92a41fb398ffd79
Unknown <sami.haapanen@hotmail.com>
2018-02-08 19:06:30 +0200

    first version of budgetform
    Joel and Joona contributed

--- FashionApp/web/JS/budgetform.js
+++ FashionApp/web/JS/budgetform.js
@@ -1,15 +1,31 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    const productForm = document.querySelector("#product-form");
+    let tableHeader = document.querySelector("#list-header");
+    let button = document.querySelector("#new-table");
+    let number;
+    
 
-    let productGroupData = {};
-
-    let listProducts = function(products){
-        const pglElement = document.querySelector("#product-group-list");
-
-        for(let pg of productGroups){
-            
+    let addThemes = function (amount){
+        let divs="";
+        for(i = 1; i <= amount ; i++){
+            divs+="<div class='ib'>Theme ";
+            divs+=i;
+            divs+="</div>";
         }
+        return divs;
     }
 
+    
+    button.addEventListener("click", function(){
+        number = document.getElementById("number").value;
+        //let amountOfWeeks = number.getAttribute("value");
+        tableHeader.innerHTML +=
+        `<div class="ib">id</div>
+        <div>Product group</div>`
+        + addThemes(number) +
+        `<div class="ib">Amount of styles</div>
+        <div class="ib">Average price</div>
+        <div class="ib">Average amount/style</div>
+        <div class="ib">Total sale</div>
+        <div class="ib">Average cover 50% Total</div>`
+    })
 });
