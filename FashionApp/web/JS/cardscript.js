6220652fdecd90548d4b62fbe2e88bc393b68764
Joel Vornanen <joel.vornanen2@gmail.com>
2018-02-19 13:34:45 +0200

    New branch for card table

---
+++ FashionApp/web/JS/cardscript.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+const createTable = function (let x, let y){
+    x = 3;
+    y = 5;
+    for(let i = 0; i < x; i++){
+        let z = document.querySelector("table").insertRow(i);
+        for(let c = 0; c < y; c++){
+            let v = z.insertCell(c);
+            v.
+        }
+    }
+}
+
+
+
+
+
+
+}
