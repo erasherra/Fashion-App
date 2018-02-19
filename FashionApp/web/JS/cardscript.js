75d39436f2e167323c2ab3b4af121c22960ea425
Joel Vornanen <joel.vornanen2@gmail.com>
2018-02-19 13:50:18 +0200

    Added javascript to create table

--- FashionApp/web/JS/cardscript.js
+++ FashionApp/web/JS/cardscript.js
@@ -1,19 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-const createTable = function (let x, let y){
-    x = 3;
-    y = 5;
-    for(let i = 0; i < x; i++){
-        let z = document.querySelector("table").insertRow(i);
-        for(let c = 0; c < y; c++){
-            let v = z.insertCell(c);
-            v.
+    let button = document.querySelector("#createButton");
+    button.addEventListener("click", function () {
+        x = 3;
+        y = 5;
+        for (let r = 0; r < x; r++) {
+            let z = document.querySelector("table").insertRow(r);
+            for (let c = 0; c < y; c++) {
+                let v = z.insertCell(c);
+                v.innerHTML = "Row-" + r + "Column-" + c;
+            }
         }
-    }
-}
-
-
-
-
-
-
-}
+    
+});
+});
