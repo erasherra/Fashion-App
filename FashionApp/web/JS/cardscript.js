292b54f312c25e72aca151dcc070fa1f92ae406c
Unknown <sami.haapanen@hotmail.com>
2018-02-19 14:55:56 +0200

    Js for product cards

--- FashionApp/web/JS/cardscript.js
+++ FashionApp/web/JS/cardscript.js
@@ -1,13 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
     let button = document.querySelector("#createButton");
     button.addEventListener("click", function () {
-        x = 3;
-        y = 5;
-        for (let r = 0; r < x; r++) {
-            let z = document.querySelector("table").insertRow(r);
-            for (let c = 0; c < y; c++) {
-                let v = z.insertCell(c);
-                v.innerHTML = "Row-" + r + "Column-" + c;
+        x = 2;
+        y = 2;
+        for (let rows = 0; rows < x; rows++) {
+            let z = document.querySelector("table").insertRow(rows);
+            for (let colNum = 0; colNum < y; colNum++) {
+                let v = z.insertCell(colNum);
+                v.innerHTML = "Row-" + rows + "Column-" + colNum + 
+                `<div class="aCard">
+                    <div class="article">article : </div>
+                    <input type="text" placeholder="article" class="code">
+
+                    <div class="product">product : </div>
+                    <input type="text" placeholder="product" class="name">
+                    
+
+                    <img src="johndoe.jpg" class="img">
+
+
+                    <div class="buyPrice">purchase PRICE / pc : </div>
+                    <input type="text" placeholder="price" class="buyNumber">
+
+                    <div class="buyTotal">Total € : </div>
+                    <input type="text" placeholder="total" class="buyTotalNumber">
+
+                    <div class="sellPrice">selling PRICE / pc : </div>
+                    <input type="text" placeholder="price" class="sellNumber">
+
+                    <div class="sellTotal">Total € : </div>
+                    <input type="text" placeholder="total" class="sellTotalNumber">
+
+
+                    <div class="pieces">PIECES : </div>
+                    <input type="text" placeholder="price" class="piecesNumber">
+
+                    <div class="material">MATERIAL : </div>
+                    <input type="text" placeholder="price" class="materialNames">
+
+                    <div class="colors">COLORS : </div>
+                    <input type="text" placeholder="price" class="colorNames">
+
+                    <div class="size">SIZES : </div>
+                    <input type="text" placeholder="price" class="sizeValues">
+
+
+                <div>`;
             }
         }
     
