c1b27d2252c6328867c98973e18b52d703aa5221
Sami <sami.haapanen@hotmail.com>
2018-02-27 15:40:44 +0200

    changes to new card page
    outfit numbers corrected, addable cards, add outfit button fix + some nice css ft. joel

--- FashionApp/web/JS/testCardsScript.js
+++ FashionApp/web/JS/testCardsScript.js
@@ -7,27 +7,44 @@ document.addEventListener("DOMContentLoaded", function () {
         number++;
         outfitNumber++;
         console.log(`Outfit ${outfitNumber} added.`)
-        theme.insertAdjacentHTML('beforeend', 
+        theme.insertAdjacentHTML('beforeend',
             `<div class="outfit" id="outfit${number}">
-        <div class="spaceBetween">
-            <div class="title">Outfit ${outfitNumber}</div>
-            <button class="remove" id="ob${number}">x</button>
-        </div>
-        <div class="aRow">
-            <div class="aCard">+</div>
-            <div class="aCard">+</div>
-            <div class="aCard">+</div>
-            <div class="aCard">+</div>
-            <div class="aCard">+</div>
-        </div>
-    </div>`);
+                <div class="spaceBetween">
+                    <div class="title">Outfit ${outfitNumber}</div>
+                    <button class="remove" id="ob${number}">x</button>
+                </div>
+                <div class="aRow" id="row${number}">
+                    <div class="addCard" id="addCard${number}">+</div>
+                </div>
+            </div>`);
 
-    let num = number;
+        let num = number;
         document.querySelector(`#ob${num}`).addEventListener("click", function () {
             document.querySelector(`#outfit${num}`).remove();
             console.log(`Outfit ${num} removed.`);
+            let setId = 0;
+            let titles = document.querySelectorAll(".title")
+            for (let title of titles) {
+                setId++;
+                title.innerHTML = `Outfit ${setId}`;
+            }
+        });
+
+        
+        let addCard = document.querySelector(`#addCard${num}`);
+        addCard.addEventListener("click", function () {
+            let newCard = document.createElement("div");
+            newCard.classList.add("addCard")
+            newCard.innerHTML = "hai";
+            let aRow = document.querySelector(`#row${num}`);
+            aRow.insertBefore(newCard, addCard);
+            console.log("hai");
         });
     });
+
+
+
+
 
 
 
