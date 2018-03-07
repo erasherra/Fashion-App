c25b6fb78686b7a962456cb1ccb8c1a3610b2d03
sarita khanal <saritakhanal533@gmail.com>
2018-03-07 10:46:26 +0200

    remove xml

--- FashionApp/src/java/rest/ColorDBFacadeREST.java
+++ FashionApp/src/java/rest/ColorDBFacadeREST.java
@@ -37,14 +37,14 @@ public class ColorDBFacadeREST extends AbstractFacade<ColorDB> {
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_JSON})
     public void create(ColorDB entity) {
         super.create(entity);
     }
 
     @PUT
     @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_JSON})
     public void edit(@PathParam("id") Integer id, ColorDB entity) {
         super.edit(entity);
     }
@@ -57,21 +57,21 @@ public class ColorDBFacadeREST extends AbstractFacade<ColorDB> {
 
     @GET
     @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public ColorDB find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public List<ColorDB> findAll() {
         return super.findAll();
     }
 
     @GET
     @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces({ MediaType.APPLICATION_JSON})
     public List<ColorDB> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
         return super.findRange(new int[]{from, to});
     }
